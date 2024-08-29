51e52236013b87d4e0a6b5475d02b538b2e6693b
Corbin G <corbot2004.vt@gmail.com>
2024-08-28 23:14:55 -0400

    Initial Commit

---
+++ src/main/java/net/desati/penguinese/PenguineseClient.java
@@ -0,0 +1,10 @@
+package net.desati.penguinese;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PenguineseClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/desati/penguinese/PenguineseDataGenerator.java
@@ -0,0 +1,11 @@
+package net.desati.penguinese;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PenguineseDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
