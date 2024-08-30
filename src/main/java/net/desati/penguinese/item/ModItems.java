5763ab4378b993ae44ae4241482eff126a88fa4e
Corbin G <corbot2004.vt@gmail.com>
2024-08-30 16:08:42 -0400

    Added All Shard Types

--- src/main/java/net/desati/penguinese/item/ModItems.java
+++ src/main/java/net/desati/penguinese/item/ModItems.java
@@ -14,6 +14,10 @@ import net.minecraft.util.Rarity;
 public class ModItems {
     public static final Item ICE_CUBE = registerItem("ice_cube", new Item(new Item.Settings().food(ModFoodComponents.ICE_CUBE)));
     public static final Item ICE_SHEET = registerItem("ice_sheet", new Item(new Item.Settings()));
+    public static final Item ICE_SHARD = registerItem("ice_shard", new Item(new Item.Settings()));
+    public static final Item PACKED_ICE_SHARD = registerItem("packed_ice_shard", new Item(new Item.Settings()));
+    public static final Item BLUE_ICE_SHARD = registerItem("blue_ice_shard", new Item(new Item.Settings()));
+    public static final Item BLACK_ICE_SHARD = registerItem("black_ice_shard", new Item(new Item.Settings()));
 
     public static final Item BLACK_ICE = registerItem("black_ice", new BlockItem(ModBlocks.BLACK_ICE, new Item.Settings().rarity(Rarity.UNCOMMON)));
 
@@ -27,6 +31,11 @@ public class ModItems {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
             entries.add(ICE_CUBE);
             entries.add(ICE_SHEET);
+            entries.add(ICE_SHARD);
+            entries.add(PACKED_ICE_SHARD);
+            entries.add(BLUE_ICE_SHARD);
+            entries.add(BLACK_ICE_SHARD);
+
         });
     }
 }
