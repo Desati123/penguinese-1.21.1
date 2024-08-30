c4a21e0cd781033bc2c0af7bcf62e56a45858e2c
Corbin G <corbot2004.vt@gmail.com>
2024-08-30 14:09:39 -0400

    Increase chance for Ice Cubes to Hurt when Consumed

--- src/main/java/net/desati/penguinese/item/ModFoodComponents.java
+++ src/main/java/net/desati/penguinese/item/ModFoodComponents.java
@@ -7,5 +7,5 @@ import net.minecraft.entity.effect.StatusEffects;
 public class ModFoodComponents {
     public static final FoodComponent ICE_CUBE = new FoodComponent.Builder().nutrition(0).saturationModifier(0f)
             .alwaysEdible()
-            .statusEffect(new StatusEffectInstance(StatusEffects.INSTANT_DAMAGE, 1), 0.05f).build();
+            .statusEffect(new StatusEffectInstance(StatusEffects.INSTANT_DAMAGE, 1, 1), 0.1f).build();
 }
